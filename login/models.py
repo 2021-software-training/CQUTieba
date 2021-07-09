6e0282c738865f8a6b28e4f6a9e4e8c41090fb77
liawom@foxmail.com <60684451+Aw0m@users.noreply.github.com>
2021-07-10 00:06:33 +0800

    修改了model中存在的Bug (未设定CharField中的max_length)

--- login/models.py
+++ login/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import User
 
 class MyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_id = models.PositiveIntegerField(primary_key=True)
+    my_user_id = models.PositiveIntegerField(primary_key=True)
     
     age = models.PositiveSmallIntegerField(default=0)
     gender = models.PositiveSmallIntegerField(default=0)    # 1: 男性，2：女性，0：未设定
     photo_url = models.ImageField(blank=True)
 
-    address_provinces = models.CharField(blank=True)
-    address_city = models.CharField(blank=True)
+    address_provinces = models.CharField(blank=True, max_length=10)
+    address_city = models.CharField(blank=True, max_length=10)
 
-    habits1 = models.CharField(blank=True)
-    habits2 = models.CharField(blank=True)
-    habits3 = models.CharField(blank=True)
+    habits1 = models.CharField(blank=True, max_length=10)
+    habits2 = models.CharField(blank=True, max_length=10)
+    habits3 = models.CharField(blank=True, max_length=10)
 
     birthday = models.DateTimeField(blank=True)
     status = models.BooleanField(default=True)
-    signature = models.CharField(default='还没有个性签名呢~')
+    signature = models.CharField(default='还没有个性签名呢~', max_length=50)
     exp_value = models.PositiveIntegerField(default=1)
 
     front_size = models.IntegerField(default=10)
