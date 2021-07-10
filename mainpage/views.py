3336e0f966827817b6cbdd8e6416bfa9f630d5fd
liawom@foxmail.com <60684451+aw0m@users.noreply.github.com>
2021-07-10 17:25:06 +0800

    新增展示api接口，返回单个文章的详细信息

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,7 @@
 import json
 from django.http import HttpResponse
+from mainpage.models import Article, LikeList
+from login.models import MyUser
 
 
 def add_article(request):
@@ -24,3 +26,41 @@ def add_comment(request):
         }
     :return:
     """
+
+
+def show_an_article(request):
+    if request.method == "GET":
+        article_id = int(request.GET["articleID"])
+        try:
+            article = Article.objects.get(pk=article_id)
+            author_id = article.author_id
+            user = MyUser.objects.get(my_user_id=author_id)
+
+            # 判断是否已经有点赞了
+            temp = LikeList.objects.filter(article_id=article_id, author_id=author_id)
+            likes_judge = bool(temp)
+
+            data = {
+                "userName": user.user.username,
+                "authorID": user.my_user_id,
+                "articleID": article.article_id,
+                "articleText": article.article_text,
+                "articleViews": article.article_views,
+                "articleTime": str(article.article_time),
+                # "articleAudio": article_audio,
+                "articleTitle": article.article_title,
+                "likesNum": article.likes_num,
+                "commentsNum": article.comments_num,
+                "articleType1": article.article_type1,
+                "articleType2": article.article_type2,
+                "article_type3": article.article_type3,
+                "likeJudge": likes_judge
+            }
+            article.article_views += 1
+            article.save()
+            return HttpResponse(json.dumps({"result": data}), 'application/json')
+        except Article.DoesNotExist:
+            return HttpResponse(json.dumps({"result": "does not exist"}), 'application/json')
+    else:
+        return HttpResponse(json.dumps({"result": "not GET"}), 'application/json')
+    pass
