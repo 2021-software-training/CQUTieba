f1787f32cd8167fca3cae386d89d57ba4da3a12e
Knowbody-arc <2669188718@qq.com>
2021-07-10 17:37:56 +0800

    加评论

--- mainpage/views.py
+++ mainpage/views.py
@@ -7,20 +7,24 @@ from django.http import HttpResponse,HttpResponseRedirect
 '''
 
 def add_comment(request):#第一次加入文章
-    if(request.method=="GET"):
-        comment_text=request.GET(['comment_text'])
-        commenter_id=request.GET(['commenter_id'])
-        article_id=request.GET(['article_id'])
-        likes_num=0
-        comment_audio=request.GET(['comment_audio'])
-        comment_time=request.GET(['comment_time'])
-        comment_text+="\n"+"{0} 回复 {1}".format(commenter_id,article_id)
-        comment_this=Comment(
+    """
+       :param request: {
+           commentText, commentID, articleID
+               commentAudio(先默认为空)
+           }
+       :return:
+       """
+    if request.method == "GET":
+        comment_text = request.GET(['comment_text'])
+        commenter_id = request.GET(['commenter_id'])
+        article_id = request.GET(['article_id'])
+        comment_text += "\n"+"{0} 回复 {1}".format(commenter_id,article_id)
+        comment_this = Comment(
             comment_text,commenter_id,article_id,
             likes_num,comment_audio,comment_time
         )
         comment_this.save()#加入到数据库当中去
-        return HttpResponse(json.dumps({"result": "yes","commenter":commenter_id,"comment_target":article_id}))
+        return HttpResponse(json.dumps({"result": "yes","commenter":commenter_id}))
     return HttpResponse(json.dumps({"result":"no"}))
 
 
