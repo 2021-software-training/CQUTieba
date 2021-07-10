71710cb9357b2fa623a137049b56d8bacbe2f3f6
Knowbody-arc <2669188718@qq.com>
2021-07-10 17:41:15 +0800

    加评论

--- mainpage/views.py
+++ mainpage/views.py
@@ -21,7 +21,7 @@ def add_comment(request):#第一次加入文章
         comment_text += "\n"+"{0} 回复 {1}".format(commenter_id,article_id)
         comment_this = Comment(
             comment_text,commenter_id,article_id,
-            likes_num,comment_audio,comment_time
+            comment_audio
         )
         comment_this.save()#加入到数据库当中去
         return HttpResponse(json.dumps({"result": "yes","commenter":commenter_id}))
