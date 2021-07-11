03d1a7c11fdf724746fa8065383e5dfeefd9957c
liawom@foxmail.com <60684451+aw0m@users.noreply.github.com>
2021-07-11 15:15:59 +0800

    简单修改了url访问的路径

--- CQUTieba/urls.py
+++ CQUTieba/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login.urls')),
-    path('mainpage/', include('mainpage.urls'))
+    path('mainPage/', include('mainpage.urls'))
 ]
